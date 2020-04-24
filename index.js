e77b565d823c1cc943a468cdf9a2257908511fd6
Nisbij <42967110+Nisbij@users.noreply.github.com>
2020-04-24 23:22:15 +0530

    Third one

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Archelios:N!5@rg11232@cluster0-becy8.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-app.use('/static', express.static(path.join(__dirname, 'views')))
+app.use('/static', express.static(__dirname+ 'views'))
 client.connect(err => {
   
   // perform actions on the collection object
@@ -11,7 +11,7 @@ client.connect(err => {
     var collection = mongodb.db("form").collection("user_info");
     console.log(req);
     collection.insertOne(req);
-    
+    res.sendFile(__dirname + "/views/1.html");
   })
   client.close();
 });
